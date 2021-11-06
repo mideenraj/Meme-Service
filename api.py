ac27ca7a3044c1b047146763e77fbfc9049d934f
Rebecca Paolucci <repaolucci@gmail.com>
2021-11-06 13:19:14 -0500

    Initial commit

---
+++ api.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_restful import Resource, Api
+import os, requests, random
+
+app = Flask(__name__)
+api = Api(app)
+
+class MemeService(Resource):
+    
+    def get(self):
+        req = requests.get('http://alpha-meme-maker.herokuapp.com/')
+        memes = req.json()['data']
+        random_meme = random.choice(memes)
+        return random_meme
+        
+api.add_resource(MemeService, '/meme')
+
+
+
+if __name__ == '__main__':
+    app.run(port=7000, debug=True)
+    
